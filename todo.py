a16690d7701228dcb1aec858a62408557d1115ed
Shehma Ali <shehmaali08@gmail.com>
2025-07-18 06:01:06 +0500

    starting point

---
+++ todo.py
@@ -0,0 +1,26 @@
+"""
+🟡🟣🟠🔴🔵🟢💻😒👍😊🐚🪶🦋🐦✨✨
+personal finance manager
+■STEPS■
+🔵Have some premade spending and ask the user for their other personal spending
+🔵get the value input
+🔵 Use Google Gemini API for the current rates
+🔵give the user their spending, their savings,
+and provide them multiple ongoing offers that could save them money
+🔵also offer a money box that would tell them how much savings they have
+and they would need to buy something specific for themselves
+
+■IMPORTANT THINGS TO REMEMBER■
+🔴This must be a full-stack project
+🔴This must involve OOPs
+🔴This must use integration (mainly Gemini)
+🔴This must be made this year
+⚫could be deployed (or not)
+
+✨ 50/30/20 rule
+✨ 70/20/10 rule
+✨ 30/40/30 rule
+"""
+# input
+name = input("Hi, What is your name? ")
+User_Money = input(f"{name} can you enter the exact number of the allowance you have this month (with your currency symbol): ")
